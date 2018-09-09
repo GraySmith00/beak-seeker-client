f50a0e6371fae9b4ba275a63e04c86b171b00f11
Gray Smith <gray.smith00@gmail.com>
2018-09-08 20:43:06 -0600

    Build out Hotspot component

--- src/containers/Hotspot/Hotspot.js
+++ src/containers/Hotspot/Hotspot.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import BirdInfo from '../BirdInfo/BirdInfo';
+import { dashCaseNameHelper } from '../../utils/helpers';
+
 class Hotspot extends Component {
   render() {
+    const { hotspot } = this.props;
     const { locName, birds } = this.props.hotspot;
     const displayBirds = birds
       .map((bird, index) => (
-        <p key={`${index}-${bird.speciesCode}`} className="bird">
-          {bird.comName}
-        </p>
+        <div key={`${index}-${bird.speciesCode}`} className="bird">
+          <Link
+            to={`/hotspots/${dashCaseNameHelper(
+              hotspot.locName
+            )}/${dashCaseNameHelper(bird.comName)}`}
+          >
+            {bird.comName}
+          </Link>
+          <Route
+            exact
+            path={`/hotspots/${dashCaseNameHelper(
+              hotspot.locName
+            )}/${dashCaseNameHelper(bird.comName)}`}
+            render={() => <BirdInfo bird={bird} />}
+          />
+        </div>
       ))
       .slice(0, 10);
     return (
-      <div>
-        <h1>{locName}</h1>
-        <div className="birds">{displayBirds}</div>
-      </div>
+      <Router>
+        <div>
+          <h1>{locName}</h1>
+          <div className="birds">{displayBirds}</div>
+        </div>
+      </Router>
     );
   }
 }
