291cec78126251f355679195d093b728fedbfe65
Gray Smith <gray.smith00@gmail.com>
2018-09-14 22:00:06 -0600

    Post to twitter working with signed in user! 🔥 still need to make status' dynamic

--- src/containers/Hotspot/Hotspot.js
+++ src/containers/Hotspot/Hotspot.js
@@ -4,6 +4,19 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 export class Hotspot extends Component {
+  postTweet = async () => {
+    const url = 'http://localhost:5000/twitter/posttweet';
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(this.props.currentUser),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    // const data = await response.json();
+    console.log(response);
+  };
+
   render() {
     const { hotspotId } = this.props;
     const hotspot = this.props.hotspots.find(
@@ -25,6 +38,7 @@ export class Hotspot extends Component {
       <div>
         <h1>{locName}</h1>
         <div className="birds">{displayBirdLinks}</div>
+        <button onClick={this.postTweet}>Tweet this</button>
       </div>
     );
   }
@@ -32,11 +46,13 @@ export class Hotspot extends Component {
 
 Hotspot.propTypes = {
   hotspotId: PropTypes.string.isRequired,
-  hotspots: PropTypes.array.isRequired
+  hotspots: PropTypes.array.isRequired,
+  currentUser: PropTypes.object
 };
 
 export const mapStateToProps = state => ({
-  hotspots: state.hotspots
+  hotspots: state.hotspots,
+  currentUser: state.currentUser
 });
 
 export default connect(mapStateToProps)(Hotspot);
