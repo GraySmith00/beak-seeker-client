c0e3e2f23df34e2f155c1eb3ce96156244cf232f
Gray Smith <gray.smith00@gmail.com>
2018-09-16 10:38:51 -0600

    Build out Fixed bottom footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,17 +6,17 @@ import './Footer.css';
 const Footer = props => {
   return (
     <footer className="footer">
-      <NavLink exact to={'/home'}>
+      <NavLink exact to={'/home'} className="nav-link">
         <p>
           <i className="fas fa-home" />
         </p>
       </NavLink>
-      <NavLink exact to={'/hotspots'}>
+      <NavLink exact to={'/hotspots'} className="nav-link">
         <p>
           <i className="fas fa-map-marked-alt" />
         </p>
       </NavLink>
-      <NavLink exact to={'/logout'}>
+      <NavLink exact to={'/logout'} className="nav-link">
         <p>
           <i className="fas fa-sign-out-alt" />
         </p>
