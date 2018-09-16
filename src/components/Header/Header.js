0e8a2a8c4aad9acdc690d3f53d69b8d695d8b721
Gray Smith <gray.smith00@gmail.com>
2018-09-16 10:38:33 -0600

    Build out Header component with back button

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+
+import './Header.css';
+
+const Header = ({ currentPage, history }) => {
+  return (
+    <div className="header">
+      <span onClick={history.goBack} className="back-icon">
+        <i className="fas fa-angle-left" />
+      </span>
+      <h3>{currentPage}</h3>
+    </div>
+  );
+};
+
+export default withRouter(Header);
