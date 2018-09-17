3155cda5f56552f1f1aad5a56df9aa3f7ac997c4
Gray Smith <gray.smith00@gmail.com>
2018-09-17 13:10:13 -0600

    Refactor out initial fetch call from getNearbyHotspots

--- src/actions/thunks/getNearbyHotspots.js
+++ src/actions/thunks/getNearbyHotspots.js
@@ -3,25 +3,17 @@ import { hotspotsErrored, hotspotsSuccess } from '../hotspotActions';
 import {
   getHotspotBirds,
   getMostActive,
-  getPosition
-} from '../../utils/helpers';
-import { eBirdKey } from '../../keys';
+  getPosition,
+  getHotspotData
+} from '../../utils/apiCalls';
 
 export const getNearbyHotspots = () => async dispatch => {
-  const position = await getPosition();
-  const { latitude, longitude } = position.coords;
-
-  const url = `https://ebird.org/ws2.0/ref/hotspot/geo?lat=${latitude}&lng=${longitude}&fmt=json&dist=10`;
-
   try {
-    const response = await fetch(url, {
-      headers: {
-        'x-ebirdapitoken': eBirdKey
-      }
-    });
-    const hotspotData = await response.json();
-    const hotSpotWithBirdsPromises = await getHotspotBirds(hotspotData);
-    const hotSpotsWithBirds = await Promise.all(hotSpotWithBirdsPromises);
+    const position = await getPosition();
+    const { latitude, longitude } = position.coords;
+    const hotspotData = await getHotspotData(latitude, longitude);
+    const hotSpotsWithBirds = await getHotspotBirds(hotspotData);
+    console.log(hotSpotsWithBirds);
     const mostActive = getMostActive(hotSpotsWithBirds);
 
     dispatch(hotspotsSuccess(mostActive));
