1ac9e5e57bd528f91d6e35fbeb6f85cc240d7959
Gray Smith <gray.smith00@gmail.com>
2018-09-18 14:30:35 -0600

    Remove console log and update snapshots

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ import { withRouter } from 'react-router';
 import './Header.css';
 
 const Header = ({ currentPage, history }) => {
-  console.log(history);
   return (
     <div className="header">
       <span onClick={history.goBack} className="back-icon">
