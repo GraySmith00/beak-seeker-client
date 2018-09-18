ed617970e68b4b9eefe395dc3bc6930943edf5d5
Gray Smith <gray.smith00@gmail.com>
2018-09-17 18:33:18 -0600

    Mock out getBirdImage and getBirdInfo

--- src/utils/__mocks__/apiCalls.js
+++ src/utils/__mocks__/apiCalls.js
@@ -1,1 +1,7 @@
-export const getBirdImage = jest.fn();
+export const getBirdImage = () => {
+  return 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Kanadagans_Branta_canadensis.jpg/200px-Kanadagans_Branta_canadensis.jpg';
+};
+
+export const getBirdInfo = () => {
+  return 'The Canada goose (Branta canadensis) is a large wild goose species with a black head and neck, white cheeks, white under its chin, and a brown body.';
+};
