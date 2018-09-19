a0498ddeae68455bf49656d61317fdd68fe78076
Gray Smith <gray.smith00@gmail.com>
2018-09-18 22:17:31 -0600

    Test BirdInfo mapStateToProps

--- src/containers/BirdInfo/BirdInfo.test.js
+++ src/containers/BirdInfo/BirdInfo.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { BirdInfo } from './BirdInfo';
+import { BirdInfo, mapStateToProps } from './BirdInfo';
 
 import { mockBird, mockHotspotsWithBirds } from './mockBirdInfoData';
 
@@ -32,6 +32,10 @@ describe('BirdInfo component', () => {
     };
   });
 
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('should have an initial local state', () => {
     const expected = {
       comName: '',
@@ -46,6 +50,16 @@ describe('BirdInfo component', () => {
   it('should set the state on componentDidMount', async () => {
     await wrapper.instance().componentDidMount();
     expect(wrapper.state()).toEqual(expected);
+  });
+
+  it.skip('should render the proper text if there is no bird image', () => {
+    wrapper.setState({
+      birdImage: null
+    });
+
+    expect(wrapper.find('.not-found-text').text()).toEqual(
+      'Sorry, we could not find an image of this bird at this time :('
+    );
   });
 
   describe('loadBirdInfo', () => {
@@ -65,7 +79,18 @@ describe('BirdInfo component', () => {
     });
   });
 
-  it('should match the snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+  describe('mapStateToProps', () => {
+    it('should return an object with a hotspots array', () => {
+      const expected = {
+        hotspots: mockHotspotsWithBirds
+      };
+      const mockStore = {
+        hotspots: mockHotspotsWithBirds
+      };
+
+      const mappedProps = mapStateToProps(mockStore);
+
+      expect(mappedProps).toEqual(expected);
+    });
   });
 });
