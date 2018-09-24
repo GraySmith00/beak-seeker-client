3fb913a602e88859df9f755cf41ae94137a917f6
Gray Smith <gray.smith00@gmail.com>
2018-09-23 21:00:40 -0600

    Build out basic MyHotspots component shell

---
+++ src/containers/MyHotspots/MyHotspots.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyHotspots extends Component {
+  render() {
+    return (
+      <div>
+        <h1>MyHotspots</h1>
+      </div>
+    );
+  }
+}
+
+export default MyHotspots;
