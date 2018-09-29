f4750856e89bc6c95cc57ab073938355cab67a75
Gray Smith <gray.smith00@gmail.com>
2018-09-28 22:07:06 -0600

    Build out initial MyHotspots component and styling

--- src/containers/MyHotspots/MyHotspots.js
+++ src/containers/MyHotspots/MyHotspots.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { getMyHotspots } from '../../actions/thunks/getMyHotspots';
+
+import Header from '../../components/Header/Header';
+import './MyHotspots.css';
 
 class MyHotspots extends Component {
+  async componentDidMount() {
+    const { currentUser } = this.props;
+    const ids = currentUser.sightings.reduce((ids, sighting) => {
+      if (!ids[sighting.locationId]) {
+        ids[sighting.locationId] = null;
+      }
+      return ids;
+    }, {});
+    await getMyHotspots(Object.keys(ids));
+  }
+
   render() {
-    console.log('hiii');
+    const { currentUser } = this.props;
+
+    const displayHotspotLinks = currentUser.sightings.map((sighting, i) => (
+      <Link
+        key={`${sighting.locationId}-${i}`}
+        className="hotspot"
+        to={`/hotspots/${sighting.locationId}`}
+      >
+        {sighting.locationName}
+      </Link>
+    ));
+
     return (
       <div className="my-hotspots">
-        <h1>MyHotspots</h1>
+        <Header currentPage="My Hotspots" />
+        <main className="main-content">
+          <h1>My Hotspots</h1>
+          {displayHotspotLinks}
+        </main>
       </div>
     );
   }
 }
 
-export default MyHotspots;
+export const mapStateToProps = state => ({
+  currentUser: state.currentUser
+});
+
+export default connect(mapStateToProps)(MyHotspots);
