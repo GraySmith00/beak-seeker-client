eecc765aa940cc96b06b37d8dc6e227e96d0c3d9
Gray Smith <gray.smith00@gmail.com>
2018-09-29 20:27:45 -0600

    Finish out getMyHotspots thunk, mHotspots populating in store

--- src/actions/thunks/getMyHotspots.js
+++ src/actions/thunks/getMyHotspots.js
@@ -3,9 +3,7 @@ import { setMyHotspotsState } from '../myHotspotsActions';
 import { getMyHotspotsData, getHotspotBirds } from '../../utils/apiCalls';
 
 export const getMyHotspots = locIds => async dispatch => {
-  console.log('hiiiii');
   const hotspotsData = await getMyHotspotsData(locIds);
   const hotspotsWithBirds = await getHotspotBirds(hotspotsData);
-  console.log(getHotspotBirds);
   dispatch(setMyHotspotsState(hotspotsWithBirds));
 };
