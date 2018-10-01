39a2623c800dfbc7122a5ef4e7821f0407593d7a
Gray Smith <gray.smith00@gmail.com>
2018-09-30 19:00:23 -0600

    Style BadgeModal and add Tweet box

--- src/containers/BadgeModal/BadgeModal.js
+++ src/containers/BadgeModal/BadgeModal.js
@@ -1,37 +1,59 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './BadgeModal.css';
 
 class BadgeModal extends Component {
-  render() {
-    const { numSighted, handleModalClose, locName } = this.props;
+  constructor() {
+    super();
+    this.state = {
+      tweetText: '',
+      badge: ''
+    };
+  }
 
-    let displayTrophy;
-    let badgeText;
+  componentDidMount() {
+    const { numSighted, locName } = this.props;
+    let badge;
 
     if (numSighted === 6) {
-      displayTrophy = (
-        <i className="fas fa-trophy" style={{ color: 'silver' }} />
-      );
-      badgeText = 'silver';
+      badge = 'silver';
     } else if (numSighted === 8) {
-      displayTrophy = <i className="fas fa-trophy" style={{ color: 'gold' }} />;
-      badgeText = 'gold';
+      badge = 'gold';
     }
+
+    const tweetText = `Just earned a new ${badge} badge on @beakseeker for sighting ${numSighted} birds at ${locName}!!!`;
+
+    this.setState({ tweetText, badge });
+  }
+
+  render() {
+    const { numSighted, handleModalClose, locName } = this.props;
+    const { badge } = this.state;
 
     return (
       <div className="badge-modal">
         <div className="modal-inner">
           <h1>Congratulations!</h1>
-          {displayTrophy}
+          <i className="fas fa-trophy" style={{ color: badge }} />
           <div className="modal-card-bottom">
             <p
-            >{`You've earned a ${badgeText} badge for sighting ${numSighted} birds at ${locName}!`}</p>
+            >{`You've earned a ${badge} badge for sighting ${numSighted} birds at ${locName}!`}</p>
             <div className="modal-buttons">
               <button onClick={handleModalClose} className="close">
                 Close
               </button>
-              <button className="tweet-badge">Tweet this Badge</button>
               <button className="view-badges">View my Badges</button>
+            </div>
+            <div className="tweet-badge">
+              <textarea
+                className="text-box"
+                onChange={this.handleChange}
+                type="text"
+                name="tweetText"
+                rows="3"
+                value={this.state.tweetText}
+              />
+              <button className="tweet-button">Tweet this Badge</button>
             </div>
           </div>
         </div>
