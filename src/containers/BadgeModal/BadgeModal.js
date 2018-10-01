de422766c0d341320f7842c8a5c605d370a4f4c8
Gray Smith <gray.smith00@gmail.com>
2018-09-30 18:05:47 -0600

    Build out BadgeModal and styling, still need to style the buttons

--- src/containers/BadgeModal/BadgeModal.js
+++ src/containers/BadgeModal/BadgeModal.js
@@ -3,10 +3,38 @@ import './BadgeModal.css';
 
 class BadgeModal extends Component {
   render() {
+    const { numSighted, handleModalClose, locName } = this.props;
+
+    let displayTrophy;
+    let badgeText;
+
+    if (numSighted === 6) {
+      displayTrophy = (
+        <i className="fas fa-trophy" style={{ color: 'silver' }} />
+      );
+      badgeText = 'silver';
+    } else if (numSighted === 8) {
+      displayTrophy = <i className="fas fa-trophy" style={{ color: 'gold' }} />;
+      badgeText = 'gold';
+    }
+
     return (
       <div className="badge-modal">
-        <h1>BadgeModal</h1>
-        <p onClick={this.props.handleModalClose}>x</p>
+        <div className="modal-inner">
+          <h1>Congratulations!</h1>
+          {displayTrophy}
+          <div className="modal-card-bottom">
+            <p
+            >{`You've earned a ${badgeText} badge for sighting ${numSighted} birds at ${locName}!`}</p>
+            <div className="modal-buttons">
+              <button onClick={handleModalClose} className="close">
+                Close
+              </button>
+              <button className="tweet-badge">Tweet this Badge</button>
+              <button className="view-badges">View my Badges</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
