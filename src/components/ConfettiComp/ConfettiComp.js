075edcf0e4d8b6411699685984b8b8f92f5a9fe6
Gray Smith <gray.smith00@gmail.com>
2018-09-30 21:17:02 -0600

    Add react-confetti ConfettiComp component

---
+++ src/components/ConfettiComp/ConfettiComp.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import sizeMe from 'react-sizeme';
+import Confetti from 'react-confetti';
+
+const ConfettiComp = sizeMe({
+  monitorHeight: true,
+  monitorWidth: true
+})(
+  class Example extends React.PureComponent {
+    static propTypes = {
+      size: PropTypes.shape({
+        width: PropTypes.number,
+        height: PropTypes.number
+      })
+    };
+    render() {
+      return (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          }}
+        >
+          <Confetti {...this.props.size} recycle={false} numberOfPieces={350} />
+        </div>
+      );
+    }
+  }
+);
+
+export default ConfettiComp;
